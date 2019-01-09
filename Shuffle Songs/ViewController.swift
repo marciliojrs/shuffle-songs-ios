2dbb5920e5065b123fc249643fc57eeb7cd22f67
Marcilio Junior <marciliojrs@gmail.com>
2019-01-09 12:23:41 -0200

    Initial Commit

---
+++ Shuffle Songs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shuffle Songs
+//
+//  Created by Marcilio Junior on 09/01/19.
+//  Copyright © 2019 Marcilio Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Shuffle SongsTests/Shuffle_SongsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Shuffle_SongsTests.swift
+//  Shuffle SongsTests
+//
+//  Created by Marcilio Junior on 09/01/19.
+//  Copyright © 2019 Marcilio Junior. All rights reserved.
+//
+
+import XCTest
+@testable import Shuffle_Songs
+
+class Shuffle_SongsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
