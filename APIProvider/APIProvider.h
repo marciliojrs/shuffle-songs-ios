3f516aa3a0f8d0d8bf23f57623f5d16590606268
Marcilio Junior <marciliojrs@gmail.com>
2019-01-09 22:30:09 -0200

    API Provider files

---
+++ APIProvider/APIProvider.h
@@ -0,0 +1,19 @@
+//
+//  APIProvider.h
+//  APIProvider
+//
+//  Created by Marcilio Junior on 09/01/19.
+//  Copyright © 2019 Marcilio Junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIProvider.
+FOUNDATION_EXPORT double APIProviderVersionNumber;
+
+//! Project version string for APIProvider.
+FOUNDATION_EXPORT const unsigned char APIProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIProvider/PublicHeader.h>
+
+
