e172a3d7d155c98a1613fe1601bc2243f4ce86b2
Marcilio Junior <marciliojrs@gmail.com>
2019-01-10 19:44:34 -0200

    GetTracksByArtististWithLimitUseCase implementation

--- Domain/UseCase/GetTracksByArtistListWithLimitUseCase.swift
+++ Domain/UseCase/GetTracksByArtistListWithLimitUseCase.swift
@@ -1,5 +1,10 @@
+public typealias GetTracksByArtistListWithLimitUseCaseResponse = ((Result<[Track]>) -> Void)
+
 public protocol GetTracksByArtistListWithLimitUseCaseType {
     init(repository: MusicRepository)
+
+    func execute(artists: [Identity], limit: Int, callback: @escaping GetTracksByArtistListWithLimitUseCaseResponse)
+    func execute(artists: [Identity], callback: @escaping GetTracksByArtistListWithLimitUseCaseResponse)
 }
 
 public struct GetTracksByArtistListWithLimitUseCase: GetTracksByArtistListWithLimitUseCaseType {
@@ -7,5 +12,15 @@ public struct GetTracksByArtistListWithLimitUseCase: GetTracksByArtistListWithLi
 
     public init(repository: MusicRepository) {
         self.repository = repository
+    }
+
+    public func execute(artists: [Identity], callback: @escaping GetTracksByArtistListWithLimitUseCaseResponse) {
+        execute(artists: artists, limit: 5, callback: callback)
+    }
+
+    public func execute(artists: [Identity],
+                        limit: Int,
+                        callback: @escaping GetTracksByArtistListWithLimitUseCaseResponse) {
+        repository.lookup(artistIds: artists, limit: limit, callback: callback)
     }
 }
