06036bd5a657684a9fdd3713005e6c1e71190afc
Marcilio Junior <marciliojrs@gmail.com>
2019-01-13 23:03:12 -0200

    Fade effect for image loading

--- Shuffle Songs/Util/Extension/UIImageView+Download.swift
+++ Shuffle Songs/Util/Extension/UIImageView+Download.swift
@@ -4,7 +4,12 @@ extension UIImageView {
     func downloadImage(from url: URL) {
         self.image = nil
         ImageLoader.shared.loadImage(from: url) { [weak self] (image) in
+            self?.alpha = 0
             self?.image = image
+
+            UIView.animate(withDuration: 0.2, animations: {
+                self?.alpha = 1
+            })
         }
     }
 }
